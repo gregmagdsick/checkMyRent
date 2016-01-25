9339ee93fb46b5713d691ca7fad7f9b30dff9fef
Elena Saygo <elena.saygo@gmail.com>
2016-01-25 12:57:19 -0800

    save

--- js/submitButton.js
+++ js/submitButton.js
@@ -1,15 +1,15 @@
 function getInputForm() {
  var s1 = location.search.substring(1, location.search.length).split('&'),
      r = {}, s2, i;
- for (var i = 0; i < s1.length; i++) {
+ for (i = 0; i < s1.length; i++) {
      s2 = s1[i].split('=');
      r[decodeURIComponent(s2[0]).toLowerCase()] = decodeURIComponent(s2[1]);
  }
  return r;
-}
+};
 
 var QueryString = getInputForm();
 
-var typeResidence = QueryString['residenceType'];
-var monthlyRent = QueryString['rent'];
-console.log(rent);
+var typeResidence = QueryString["residenceType"];
+var monthlyRent = QueryString["rent"];
+console.log(monthlyRent);
