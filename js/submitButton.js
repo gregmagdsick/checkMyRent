49ee419b77a593bad8af951819ba58d20f133996
Elena Saygo <elena.saygo@gmail.com>
2016-01-25 13:24:26 -0800

    save before checkout

--- js/submitButton.js
+++ js/submitButton.js
@@ -1,18 +1,3 @@
-// function getInputForm() {
-//  var s1 = location.search.substring(1, location.search.length).split('&'),
-//      r = {}, s2, i;
-//  for (i = 0; i < s1.length; i++) {
-//      s2 = s1[i].split('=');
-//      r[decodeURIComponent(s2[0]).toLowerCase()] = decodeURIComponent(s2[1]);
-//  }
-//  return r;
-// };
-//
-// var QueryString = getInputForm();
-//
-// var typeResidence = QueryString["residenceType"];
-// var monthlyRent = QueryString["rent"];
-// console.log(monthlyRent);
 
 function getRequests() {
     var s1 = location.search.substring(1, location.search.length).split('&'),
@@ -27,6 +12,7 @@ var QueryString = getRequests();
 
 
 var rent = QueryString["rent"];
+console.log(rent);
 var sqFeet = QueryString["sq-feet"];
 var beds = QueryString["beds"];
 var baths = QueryString["baths"];
