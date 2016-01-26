e3f06998c14024d1f75504a378b23dd3c65eaccf
Greg Magdsick <greg.magdsick@gmail.com>
2016-01-25 22:00:37 -0800

    maptest.html shows map centered on lat/lng, with marker on position

---
+++ js/mapTest.js
@@ -0,0 +1,15 @@
+var mapCenterLat = 47.6235481;
+var mapCenterLng = -122.3384007;
+
+function initMap() {
+  var mapDiv = document.getElementById('map');
+  var map = new google.maps.Map(mapDiv, {
+    center:{lat: mapCenterLat, lng: mapCenterLng},
+    zoom: 15
+  });
+
+  var marker = new google.maps.Marker({
+    position: {lat: mapCenterLat, lng: mapCenterLng}
+  })
+  marker.setMap(map);
+}
