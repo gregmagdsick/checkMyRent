22d42d72b87685247b63063a7e55fa919bea74e5
Greg Magdsick <greg.magdsick@gmail.com>
2016-01-26 09:58:46 -0800

    google map now displays result from Zillow query

--- js/mapTest.js
+++ js/mapTest.js
@@ -1,7 +1,7 @@
-var mapCenterLat = 47.6235481;
-var mapCenterLng = -122.3384007;
 
 function initMap() {
+  var mapCenterLat = 47.6235481;
+  var mapCenterLng = -122.3384007;
   var mapDiv = document.getElementById('map');
   var map = new google.maps.Map(mapDiv, {
     center:{lat: mapCenterLat, lng: mapCenterLng},
