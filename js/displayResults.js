1d43bf5d757e1120984b749c60e247b8d2bdda04
Frazier Mork <fraziermork@gmail.com>
2016-01-26 15:46:43 -0800

    added some styling to results page to make it prettier and built the results function to compare rent with zillow estimate

--- js/displayResults.js
+++ js/displayResults.js
@@ -1,6 +1,15 @@
 window.addEventListener('load', function(){
   var currentLat = Number(localStorage.getItem('mostRecentLat'));
   var currentLng = Number(localStorage.getItem('mostRecentLng'));
+  var mostRecentCounter = localStorage.getItem('mostRecentCounter');
+  var mostRecentRentEstimate = Number(localStorage.getItem('mostRecentRentEstimate'));
+  var mostRecentProperty = JSON.parse(localStorage.getItem('property' + mostRecentCounter));
+  var mostRecentRent = Number(mostRecentProperty['rent']);
+  console.log('property' + mostRecentCounter);
+  console.dir(mostRecentProperty)
+  console.log(mostRecentRent);
+  console.log(mostRecentRentEstimate);
+  compareRentAndEstimate(mostRecentRent, mostRecentRentEstimate);
   console.log(currentLat);
   console.log(currentLng);
   initMap(currentLat, currentLng);
@@ -18,4 +27,45 @@ function initMap(currentLat, currentLng) {
     position: {lat: currentLat, lng: currentLng}
   })
   marker.setMap(map);
+}
+
+function compareRentAndEstimate(rent, rentEstimate){
+  var arrowArray = [['up-arrow.png', 'Your rent is too damn high!'], ['up_side_arrow.png', 'You are probably paying too much for rent.'], ['side-arrow.png', 'You are getting an average deal on rent.'], ['down_side_arrow.png', 'You are getting a pretty good deal on rent.'], ['down-arrow.png', 'Your rent is too damn low!']];
+  var resultsHolderEl = document.getElementById('resultsHolder');
+  var arrowImgEl = document.createElement('img');
+  arrowImgEl.className = 'resultsArrow';
+  var resultsTextHolderEl = document.createElement('div');
+  resultsTextHolderEl.className = 'resultsTextHolderEl'
+  var resultsHeaderEl = document.createElement('h1');
+  resultsHeaderEl.className = 'resultsHeader';
+  var rentComparisonEl = document.createElement('h5');
+  rentComparisonEl.className = 'rentComparison';
+  rentComparisonEl.textContent = "Your rent: $" + rent + ", Zillow's estimated rent: $" + rentEstimate;
+  if (rent > 0.95 * rentEstimate && rent < 1.05 * rentEstimate){
+    //they are equal
+    arrowImgEl.src = 'img/' + arrowArray[2][0];
+    resultsHeaderEl.textContent = arrowArray[2][1];
+  } else if ( rent > 1.15 * rentEstimate){
+    //you are paying too much
+    arrowImgEl.src = 'img/' + arrowArray[0][0];
+    resultsHeaderEl.textContent = arrowArray[0][1];
+  } else if (rent < 0.85 * rentEstimate){
+    //great deal
+    arrowImgEl.src = 'img/' + arrowArray[4][0];
+    resultsHeaderEl.textContent = arrowArray[4][1];
+  } else if (rent > 1.05 * rentEstimate){
+    //paying a bit too much
+    arrowImgEl.src = 'img/' + arrowArray[1][0];
+    resultsHeaderEl.textContent = arrowArray[1][1];
+  } else if (rent < 0.95 * rentEstimate) {
+    //getting a goodish deal
+    arrowImgEl.src = 'img/' + arrowArray[3][0];
+    resultsHeaderEl.textContent = arrowArray[3][1];
+  } else {
+    alert('error!');
+  }
+  resultsHolderEl.appendChild(arrowImgEl);
+  resultsHolderEl.appendChild(resultsTextHolderEl);
+  resultsTextHolderEl.appendChild(resultsHeaderEl);
+  resultsTextHolderEl.appendChild(rentComparisonEl);
 }
