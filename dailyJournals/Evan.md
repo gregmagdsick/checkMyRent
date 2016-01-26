547f79008da5f3ac4c2e44a90c94238187ad5376
Evan Tatum <evantatum@hotmail.com>
2016-01-26 09:01:09 -0800

    work in progress

---
+++ dailyJournals/Evan.md
@@ -0,0 +1,5 @@
+*Worked on History.Html and History.Js until lunch when we had the merge Error
+*Got history.html to work/ but was later changed to fit the form that was filled for Elena
+*Worked with Elena to get the submit.button working so I can link the new history.js
+*Now the userZip userbeds userbaths usersqFeet userAddress and usersMonthlyRent all display in the resources tab in the console... but they do not update when informatin is applied to the form, and the form is gone when "submit is clicked".
+rubber ducked all the XML .ajax and shit 
---
+++ history.js
@@ -0,0 +1,64 @@
+
+var x = document.getElementById('button')
+
+x.addEventListiner('click', function(event){
+  getElementById('userAddress')
+  getElementById('userZip')
+  getElementById('userbeds')
+  getElementById('usersqFeet')
+  getElementById('userbaths')
+  getElementById('usersMonthlyRent')
+})
+
+
+
+
+var address =[];
+var zip = [];
+var sqFeet = [];
+var beds = [];
+var baths = [];
+var monthlyRent = [];
+
+
+
+var addressInput  = document.getElementById("address");
+var zipInput   = document.getElementById("zip");
+var sqFeettInput = document.getElementById("sqFeet");
+var bedsInput  = document.getElementById("beds");
+var bathInput   = document.getElementById("bath");
+var monthlyRentInput = document.getElementById("monthlyRent");
+
+
+var messageBox  = document.getElementById("display");
+
+function insert ( ) {
+ address.push( addressInput.value );
+ zip.push( zipInput.value );
+ sqFeet.push( sqFeet.value );
+ beds.push( bedsInput.value );
+ bath.push( bathInput.value );
+ monthlyRent.push( monthlyRent.value );
+
+ clearAndShow();
+}
+
+// function clearAndShow () {
+//   // Clear our fields
+//   addressInput.value = "";
+//   zipInput.value = "";
+//   sqFeetInput.value = "";
+//   bedsInput.value = "";
+//   bathInput.value = "";
+//   monthlyRentInput.value = "";
+
+  // Shows output
+//   messageBox.innerHTML = "";
+//
+//   messageBox.innerHTML += "address: " + address.join(", ") + "<br/>";
+//   messageBox.innerHTML += "zip: " + zip.join(", ") + "<br/>";
+//   messageBox.innerHTML += "sqFeet: " + sqFeet.join(", ");
+//   messageBox.innerHTML += "beds: " + beds.join(", ") + "<br/>";
+//   messageBox.innerHTML += "bath: " + bath.join(", ") + "<br/>";
+//   messageBox.innerHTML += "monthlyRent: " + monthlyRent.join(", ");
+// }
