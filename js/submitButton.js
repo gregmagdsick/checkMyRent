01c29deb65a32574711352e6032da993a916a406
Elena Saygo <elena.saygo@gmail.com>
2016-01-26 13:50:14 -0800

    work on local storage

--- js/submitButton.js
+++ js/submitButton.js
@@ -38,16 +38,30 @@ localStorage.setItem('property'+counter, JSON.stringify(objProperty));
 localStorage.setItem('counter', JSON.stringify(counter));
 }
 
+function getStorage(){
+
+ var x = localStorage.getItem('property0');
+ console.log("we got an x: ", x);
+ var y = JSON.parse(x);
+ console.log("we got and y: ", y);
+ console.log("retn is : ", y["rent"])    ;
+};
+
+var buttonToClick = document.getElementById('retrieveStorage');
+buttonToClick.addEventListener('click',getStorage);
+
 //form input validation
 //address should contain only alphanumeric characters, used regular expression  http://www.the-art-of-web.com/javascript/validate/
 
 function checkFormInput(getRentForm) {
   var address = document.getElementById('street');
-  if (address.value == "" || address.value == "Street address" ) {
-      alert('field cannot be empty!');
+  var zipCode = document.getElementById('zip');
+  if (address.value == "" || address.value == "Street address") {
+      alert('address cannot be empty!');
       address.focus();
       return false;
   }
+
   //regular expression to match only alphanumeric characters and spaces
   var addressValidation = /^[\w ]+$/;
 
@@ -59,3 +73,18 @@ function checkFormInput(getRentForm) {
 
   return true;
 }
+
+for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+  console.log( localStorage.getItem( localStorage.key( i ) ) );
+}
+//
+// function checkZipCode {
+// var zipCode = document.getElementById('zip');
+// if (zipCode.value == "" || zipCode.value == "Zip") {
+//     alert('Zip cannot be empty!');
+//     zipCode.focus();
+//     return false;
+// }
+//   var zipValidation = /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(zipCode);
+//   return true;
+// }
